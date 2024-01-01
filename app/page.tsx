a1bba0ff539e146e3c78bf05d68985a1aba2582d
Raffi Hotter <raphael.hotter@gmail.com>
2024-01-01 18:48:15 -0500

    Make shade of black lighter

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 
 const Home: NextPage = () => {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col min-h-screen text-gray-900">
       {" "}
       {/* Flex container for the entire page */}
       <main className="flex-grow">
