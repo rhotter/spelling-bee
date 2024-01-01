d84d1748cada7f90a5195b3a8c494b5339612cef
Raffi Hotter <raphael.hotter@gmail.com>
2024-01-01 18:26:12 -0500

    Slower talking speed

--- utils/speakWord.tsx
+++ utils/speakWord.tsx
@@ -1,4 +1,5 @@
 export const speakWord = (word: string): void => {
   const utterance = new SpeechSynthesisUtterance(word);
+  utterance.rate = 0.5; // Set this to a lower value for slower speech
   speechSynthesis.speak(utterance);
 };
